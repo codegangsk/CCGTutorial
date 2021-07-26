df4b34e86a3136a999e8b899889807bdc4c17cec
codegangsk <kjhj0604@gmail.com>
2021-07-26 19:41:11 +0900

    load wolf sprite and bear sprite on screen

--- CardGameV2/GameScene.swift
+++ CardGameV2/GameScene.swift
@@ -30,7 +30,12 @@ class GameScene: SKScene {
     bg.position = CGPoint.zero
     addChild(bg)
     
+    let wolf = Card(cardType: .wolf)
+    wolf.position = CGPoint(x: 100, y: 200)
+    addChild(wolf)
     
+    let bear = Card(cardType: .bear)
+    bear.position = CGPoint(x: 300, y: 200)
+    addChild(bear)
     }
-
 }
